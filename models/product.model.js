00962feeb2c2042556a2e2a7cd4dbe4f6bb6f1d4
Phuc Hau <phuchau.fit@gmail.com>
2025-08-28 11:44:14 +0700

    Thêm phần tải ảnh vào uploads

--- models/product.model.js
+++ models/product.model.js
@@ -1,5 +1,8 @@
 const mongoose = require("mongoose");
 
+const slug = require("mongoose-slug-updater")
+mongoose.plugin(slug)
+
 const productSchema = new mongoose.Schema(
     {
         title: String,
@@ -10,6 +13,11 @@ const productSchema = new mongoose.Schema(
         thumbnail: String,
         status: String,
         position: Number,
+        slug: {
+            type: String,
+            slug: "title",
+            unique: true
+        },
         deleted: {
             type: Boolean,
             default: false
